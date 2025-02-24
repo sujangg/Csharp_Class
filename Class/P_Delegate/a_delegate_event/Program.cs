cf8e35cfad367c95d269c31fdbb1f1c3e0ad86cb
WELCOME <sujangurung07.sg@gmail.com>
2025-02-24 15:27:34 +0545

    initial commit

---
+++ Class/P_Delegate/a_delegate_event/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace a_delegate_event;
+
+public delegate string MyDelegate(string str);
+
+class EventPro
+{
+    event MyDelegate MyEvent;
+
+    public EventPro()
+    {
+        this.MyEvent += new MyDelegate(this.DisplayMessage);
+
+    }
+
+    public string DisplayMessage(string message)
+    {
+        return "What's Up " + message;
+    }
+
+
+    static void Main(string[] args)
+    {
+        EventPro obj = new EventPro();
+        string result = obj.MyEvent("Sanothimi Campus");
+        System.Console.WriteLine(result);
+    }
+}
